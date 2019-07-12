952e95181935c97617f65148cd35ba93bc8ae2d0
kehonedee <38086244+kehonedee@users.noreply.github.com>
2019-07-12 11:24:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,8 +7,22 @@ app = Flask(__name__)
 def index():
     return 'Hello world!'
 
+    
+@app.route('/some-url')
+def get_data():
+    return requests.get('https://www.google.com/').content
 
 
 
-#   FROM python:3
-#   RUN pip install flask
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>get away spiders</p>"
+
+app.run()
+
