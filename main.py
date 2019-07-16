9f02cebcd690adef43dfa9a131f47a7f9795b907
kehonedee <38086244+kehonedee@users.noreply.github.com>
2019-07-16 12:53:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+
 import requests
 from flask import Flask
+import socket
+import struct
 app = Flask(__name__)
+
+#change things to make it more pro like remove "get away spiders"
+
+def ip():
+    ip = get('https://api.ipify.org').text
+    return ip
+    
+
+
+
+
+def updateduckdns(ip,ipv6):
+    domain = 'domain'
+    token = ''
+    ipv6 = 'null'
+    ipv4 = ip()
+    URL = f"https://www.duckdns.org/update?domains={domain}&token={token}[&ip={ipv4}][&ipv6={ipv6}][&verbose=true][&clear=true]"
+    requests.get(URL)
+
 
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return 'Hello worldeee!'
 
-    
-@app.route('/some-url')
+
+
+
+@app.route('/google')
 def get_data():
-    return requests.get('https://www.google.com/').content
+      return requests.get('https://www.google.com/').content
 
-
-
-
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = false
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>get away spiders</p>"
 
-app.run()
 
+app.run()
